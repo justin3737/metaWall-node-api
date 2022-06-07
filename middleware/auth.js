bbf83335c1a13ddb4f443542c891c1e6c8d86aa1
Justin Wu <juns3700@gmail.com>
2022-06-08 01:16:45 +0800

    修改名稱

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../models/users.model");
 const { handleErrorAsync, appError } = require("../service/handleError");
 const { getDecryptedJWT } = require("../service/auth");
 const { errorMsg } = require("../service/enum");
